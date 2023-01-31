195d6e74b7a716210e417fbd1d42a39e54c0ef99
thinkwithabs <twithabs@gmail.com>
2023-01-31 15:12:55 +0600

    add

--- routers/dalleRouter.js
+++ routers/dalleRouter.js
@@ -121,4 +121,27 @@ router.post("/translates",async(req,res)=>{
     }
     })
 
+
+    router.post("/recipe",async(req,res)=>{
+    try {
+        const prompt = req.body.prompt;
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 0.3,
+        max_tokens: 120,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+    
+          res.status(200).send({
+            bot: response.data.choices[0].text
+          });
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({error})
+    }
+    })
+
 export default router;
